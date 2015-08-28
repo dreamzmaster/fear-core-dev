4ec283f9989955dbe61c4d7c222c7de623b87590
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-08-28 11:33:30 +0100

    the project's gulp file uses its own lint-javascript task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
+var lint = require('./index').lintJavascript;
 
 var files = ['tasks/*.js', '*.js'];
 
-gulp.task('lint', function () {
-    return gulp.src(files)
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failOnError());
-});
+gulp.task('lint', lint(files) );
 
 gulp.task('watch', function() {
     gulp.watch(files, ['lint']);
 });
-
