55725bfc1a8dd53a034b75e6036b2731bc090bee
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-09-08 16:58:35 +0100

    wrapped copying karma.conf.js into try-catch block

--- postinstall.js
+++ postinstall.js
@@ -8,7 +8,22 @@ var fs = require('fs-extra');
 var karmaConfSrc = moduleRoot+'/karma.conf.js';
 var karmaConfDst = appRoot+'/karma.conf.js';
 
-fs.copySync(karmaConfSrc, karmaConfDst, { clobber: false }, function (err) {
-    if (err) return console.error(err);
-    console.log('copied file', karmaConfSrc, 'to', karmaConfDst);
-});
+try {
+
+    fs.copySync(karmaConfSrc, karmaConfDst, { clobber: false }, function (err) {
+        if (err) return console.error(err);
+        console.log('copied file', karmaConfSrc, 'to', karmaConfDst);
+    });
+
+} catch (e) {
+
+    if (e.message === 'EEXIST') {
+
+        console.log('karma.conf.js already exists in project root');
+
+    } else {
+
+        console.log('cannot copy karma.conf.js', e.message);
+    }
+
+}
