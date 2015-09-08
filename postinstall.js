7c2d4306ef2feb81f91e27ffa69ebe9faa8f7ec2
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-09-08 17:19:06 +0100

    postinstall tries to copy default .editorconfig to project root

--- postinstall.js
+++ postinstall.js
@@ -6,11 +6,11 @@ var moduleRoot = process.cwd();
 var fs = require('fs-extra');
 var chalk = require('chalk');
 
+var fearCoreTasks = chalk.cyan('FEAR Core tasks:');
+
 var karmaConf = 'karma.conf.js';
 var karmaConfSrc = moduleRoot+'/'+karmaConf;
 var karmaConfDst = appRoot+'/'+karmaConf;
-
-var fearCoreTasks = chalk.cyan('FEAR Core tasks:');
 
 try {
 
@@ -27,6 +27,29 @@ try {
     } else {
 
         console.log(fearCoreTasks+chalk.red(' cannot copy '+karmaConf)+'\nError: '+e.message+'\n');
+    }
+
+}
+
+var editorConfig = '.editorConfig';
+var editorConfigSrc = moduleRoot+'/'+editorConfig;
+var editorConfigDst = appRoot+'/'+editorConfig;
+
+try {
+
+    fs.copySync(editorConfigSrc, editorConfigDst, { clobber: false });
+
+    console.log(fearCoreTasks+' copied default '+chalk.green(editorConfig)+' to project root\n');
+
+} catch (e) {
+
+    if (e.message === 'EEXIST') {
+
+        console.log(fearCoreTasks+' skipped copying default '+chalk.green(editorConfig)+'\nFile already exists in project root\n');
+
+    } else {
+
+        console.log(fearCoreTasks+chalk.red(' cannot copy '+editorConfig)+'\nError: '+e.message+'\n');
     }
 
 }
