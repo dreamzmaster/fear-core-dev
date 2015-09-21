8988f6f241398f9751291b2d8dc89312f902276c
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-09-21 12:27:53 +0100

    linting result object filter does not alter message objects

--- test/tasks/helper/linting/by-rule-result-filter.spec.js
+++ test/tasks/helper/linting/by-rule-result-filter.spec.js
@@ -39,10 +39,13 @@ describe.only('linting result object filter', function () {
                         { ruleId: 'not-expected-rule-id' }
                     ]
                 };
+                var messageClone = JSON.parse(JSON.stringify(result.messages[0]));
+
                 var filtered = filter(result, 'expected-rule-id');
 
                 expect(filtered.messages.length).to.equal(1);
                 expect(filtered.messages[0].ruleId).to.equal('expected-rule-id');
+                expect(filtered.messages[0]).to.deep.equal(messageClone);
             });
 
         });
