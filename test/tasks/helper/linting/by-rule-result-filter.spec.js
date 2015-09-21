00a8a2cc9b029372c5ddc6bf34084be465e57a6d
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-09-21 13:37:15 +0100

    linting result object filter counts the errors in the filtered messages

--- test/tasks/helper/linting/by-rule-result-filter.spec.js
+++ test/tasks/helper/linting/by-rule-result-filter.spec.js
@@ -3,12 +3,16 @@
 function filter (result, ruleId) {
     var filtered = {
         filePath: result.filePath,
-        messages: []
+        messages: [],
+        errorCount: 0
     };
 
     result.messages.forEach(function (message) {
         if (message.ruleId === ruleId) {
             filtered.messages.push(message);
+            if (message.severity === 2){
+                filtered.errorCount++;
+            }
         }
     });
 
@@ -24,6 +28,16 @@ describe.only('linting result object filter', function () {
         errorCount: 0,
         warningCount: 0
     }
+
+    message {
+        ruleId: 'no-console',
+        severity: 2,
+        message: 'Unexpected console statement.',
+        line: 19,
+        column: 16,
+        nodeType: 'MemberExpression',
+        source: '               console.log(\'message\', message);'
+      }
      */
 
     it('should return a valid result object', function() {
@@ -44,7 +58,8 @@ describe.only('linting result object filter', function () {
             it('should return the original result object', function() {
                 var result = {
                     filePath: 'path/to/a/file',
-                    messages: []
+                    messages: [],
+                    errorCount: 0
                 };
                 var filtered = filter(result);
                 expect(filtered).to.deep.equal(result);
@@ -68,6 +83,17 @@ describe.only('linting result object filter', function () {
                 expect(filtered.messages.length).to.equal(1);
                 expect(filtered.messages[0].ruleId).to.equal('expected-rule-id');
                 expect(filtered.messages[0]).to.deep.equal(messageClone);
+            });
+
+            it('should count the errors in the filtered messages', function() {
+                var result = {
+                    messages: [
+                        { ruleId: 'expected-rule-id', severity: 2 },
+                        { ruleId: 'expected-rule-id', severity: 1 }
+                    ]
+                };
+                var filtered = filter(result, 'expected-rule-id');
+                expect(filtered.errorCount).to.equal(1);
             });
 
         });
