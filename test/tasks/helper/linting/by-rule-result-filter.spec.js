d60d3f0cfb19884aa3f83a616e010069b94b75a2
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-09-21 11:13:33 +0100

    linting result object filter filters out messages with unexpected ruleId

--- test/tasks/helper/linting/by-rule-result-filter.spec.js
+++ test/tasks/helper/linting/by-rule-result-filter.spec.js
@@ -1,7 +1,17 @@
 'use strict';
 
-function filter (result) {
-    return result;
+function filter (result, ruleId) {
+    var filtered = {
+        messages: []
+    };
+
+    result.messages.forEach(function (message) {
+        if (message.ruleId === ruleId) {
+            filtered.messages.push(message);
+        }
+    });
+
+    return filtered;
 }
 
 describe.only('linting result object filter', function () {
@@ -14,6 +24,22 @@ describe.only('linting result object filter', function () {
             };
             var filtered = filter(result);
             expect(filtered).to.eql(result);
+        });
+
+    });
+
+    describe('when provided with a result object having messages', function () {
+
+        it('should only return messages with the expected ruleId', function() {
+            var result = {
+                messages: [
+                    { ruleId: 'expected-rule-id' },
+                    { ruleId: 'not-expected-rule-id' }
+                ]
+            };
+            var filtered = filter(result, 'expected-rule-id');
+            expect(filtered.messages.length).to.equal(1);
+            expect(filtered.messages[0].ruleId).to.equal('expected-rule-id');
         });
 
     });
