a639c12bd079029b56daa4fc22760c79a1ba6b6b
Marton Hipszki <marton.hipszki@marks-and-spencer.com>
2015-09-23 13:39:02 +0100

    lint-javascript core task uses the new linting message filter, so linting errors can be filtered by rule

--- tasks/lint-javascript.js
+++ tasks/lint-javascript.js
@@ -2,12 +2,32 @@
 
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var getArgs = require('minimist');
+var TransformStream = require('stream').Transform;
+
+var messageFilter = require('./helper/linting/message-filter');
+
+var argv = getArgs(process.argv.slice(2));
+
+function filterResults () {
+    var stream = new TransformStream({ objectMode: true });
+
+    stream._transform = function (file, enc, cb) {
+        if (file.eslint && argv.rule) {
+            file.eslint = messageFilter(file.eslint, argv.rule);
+        }
+        cb(null, file);
+    };
+
+    return stream;
+}
 
 module.exports = function taskFactory (src, bail) {
 
     return function task () {
         return gulp.src(src)
             .pipe(eslint())
+            .pipe(filterResults())
             .pipe(eslint.format())
             .pipe(bail ? eslint.failOnError() : eslint.failAfterError());
     };
