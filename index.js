ef832885de04c6347ef1b0193e84737fc5f9ecff
alexbowen <alexbowen123@googlemail.com>
2015-11-17 16:32:16 +0000

    migrated sprites and app config tasks

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ module.exports = {
     minifyHtml : require('./tasks/build/minify-html'),
     removeHtml : require('./tasks/build/remove-html'),
     buildSetup : require('./tasks/build/setup'),
-    copy : require('./tasks/build/copy')
+    copy : require('./tasks/build/copy'),
+    createAppConfig : require('./tasks/build/create-app-config'),
+    generateSprites : require('./tasks/build/generate-sprites')
 };
---
+++ tasks/build/generate-sprites.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = function taskFactory (config, template, destination) {
+
+    return function task () {
+
+        var gulp = require('gulp');
+        var gulpif;
+        var sprity;
+
+        function loadSpriteDeps() {
+            gulpif = require('gulp-if');
+            sprity = require('sprity');
+        }
+
+        //currently not working with windows
+        if (process.platform === 'win32') {
+            return;
+        }
+
+        loadSpriteDeps();
+
+        return sprity.src({
+            src: 'app/assets/images/sprites/**/*.{png,jpg}',
+            style: 'core/sass/_generated-sprites.scss',
+            processor: 'sass',
+            cssPath: '../../../assets/images/generated/sprites/',
+            split: true,
+            margin: 0,
+            dimension: [
+                { ratio: 1, dpi: 72},
+                { ratio: 2, dpi: 192},
+                { ratio: 3, dpi: 250}
+            ]
+        }).pipe(gulpif('*.png', gulp.dest('app/assets/images/generated/sprites/'), gulp.dest('core/sass/helpers/')));
+
+    };
+};
