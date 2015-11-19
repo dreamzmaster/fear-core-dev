a0ac352a687bcdeb909a0f3f95d2d23e353307f3
nickvdyck <vandyck.nick@outlook.com>
2015-11-19 14:29:31 +0000

    moved sitespeedio task from shop-ux

---
+++ tasks/build/sitespeedio.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function (config) {
+    config = config || {};
+
+    return function task(done) {
+        var sitespeedio = require('gulp-sitespeedio'),
+            options = {
+                url: (config.url || 'http://localhost:8000/') + config.page,
+                deep: 0,
+                budget: require(config.budget).budget,
+                skipTest: config.skipTest || 'cssnumreq,cssimagesnumreq,jsnumreq,yemptysrc,ycompress,ycsstop,yjsbottom,yexpressions,ydns,yminify,redirects,noduplicates,yetags,yxhr,yxhrmethod,mindom,yno404,ymincookie,ycookiefree,ynofilter,yfavicon,thirdpartyasyncjs,cssprint,cssinheaddomain,avoidfont,totalrequests,expiresmod,longexpirehead,nodnslookupswhenfewrequests,inlinecsswhenfewrequest,textcontent,thirdpartyversions,ycdn,connectionclose,privateheaders'
+            };
+
+        return sitespeedio(options)(done);
+    };
+};
