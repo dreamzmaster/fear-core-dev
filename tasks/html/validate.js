bd27b809474f7d070f5039fdf360489d57552a9c
Ankit Singhal <asinghal@sapient.com>
2015-12-13 10:17:57 +0000

    enabled and fixed esLint

--- tasks/html/validate.js
+++ tasks/html/validate.js
@@ -1,119 +1,121 @@
-/*eslint-disenable */
 'use strict';
 
 module.exports = function(basePath, teams) {
-	config = config || {};
 
-	return function task(done) {
+    return function task(done) {
 
-		var gulp = require('gulp');
-		var gutil = require('gulp-util');
-		var childProcess = require('child_process');
-		var phantomPath = require('phantomjs').path;
-		var w3cjs = require('w3cjs');
-		var fs = require('fs');
-		var path = require('path');
-		var dir = require('node-dir');
-		var pages = [];
+        var gutil = require('gulp-util');
+        var childProcess = require('child_process');
+        var phantomPath = require('phantomjs').path;
+        var w3cjs = require('w3cjs');
+        var path = require('path');
+        var dir = require('node-dir');
+        var pages = [];
 
-		var counter = 0,
-			totalErrors = 0;
+        var counter = 0,
+            totalErrors = 0;
 
-		var openPage = function(childArgs, done) {
 
-			var pageName = childArgs[1];
+        function formatMessage(obj, done) {
+            for (var x = 0; x < obj.length; x++) {
+                if (obj[x].type === 'error') {
+                    gutil.log(gutil.colors.red('Error at line: ') + gutil.colors.blue(obj[x].lastLine) + ' ' + obj[x].message);
+                    totalErrors++;
+                }
+            }
 
-			gutil.log(gutil.colors.green('Analysing Page: ') + pageName);
+            counter++;
+            if (counter === pages.length) {
+                if (totalErrors > 0) {
+                    gutil.log(gutil.colors.red('\nTotal W3C Errors: ') + totalErrors + gutil.colors.red(' in ') + pages.length + gutil.colors.red(' pages'));
+                    done();
+                } else {
+                    done();
+                }
+            }
+        }
 
-			childProcess.execFile(phantomPath, childArgs, {
-				timeout: 60000
-			}, function(err, stdout, stderr) {
+        function openPage(childArgs, done) {
 
-				if (stderr) {
-					gutil.log(gutil.colors.red('Error getting headless data for \'%s\' (stderr): %s: ' + stderr));
-				}
+            var pageName = childArgs[1];
 
-				if (err) {
-					gutil.log(gutil.colors.red('Error analysing page: ') + pageName);
-					gutil.log(gutil.colors.red(err));
-				}
+            gutil.log(gutil.colors.green('Analysing Page: ') + pageName);
 
-				w3cjs.validate({
-					input: stdout, // file can either be a local file or a remote file
-					output: 'json', // Defaults to 'json', other option includes html
-					doctype: 'HTML5',
-					charset: 'utf-8',
-					callback: function(res) {
-						gutil.log(gutil.colors.cyan('\n--------- W3C analysis for ' + gutil.colors.green(pageName) + ' ---------'));
-						formatMessage(res.messages, done);
-					}
-				});
-			});
-		};
+            childProcess.execFile(phantomPath, childArgs, {
+                timeout: 60000
+            }, function(err, stdout, stderr) {
 
-		var formatMessage = function(obj, done) {
-			for (var x = 0; x < obj.length; x++) {
-				if (obj[x].type == 'error') {
-					gutil.log(gutil.colors.red('Error at line: ') + gutil.colors.blue(obj[x].lastLine) + ' ' + obj[x].message);
-					totalErrors++;
-				}
-			}
+                if (stderr) {
+                    gutil.log(gutil.colors.red('Error getting headless data for \'%s\' (stderr): %s: ' + stderr));
+                }
 
-			counter++;
-			if (counter === pages.length) {
-				if (totalErrors > 0) {
-					gutil.log(gutil.colors.red('\nTotal W3C Errors: ') + totalErrors + gutil.colors.red(' in ') + pages.length + gutil.colors.red(' pages'));
-					done();
-				} else {
-					done();
-				}
-			}
-		};
+                if (err) {
+                    gutil.log(gutil.colors.red('Error analysing page: ') + pageName);
+                    gutil.log(gutil.colors.red(err));
+                }
 
-		var openPages = function(pages) {
+                w3cjs.validate({
+                    input: stdout, // file can either be a local file or a remote file
+                    output: 'json', // Defaults to 'json', other option includes html
+                    doctype: 'HTML5',
+                    charset: 'utf-8',
+                    callback: function(res) {
+                        gutil.log(gutil.colors.cyan('\n--------- W3C analysis for ' + gutil.colors.green(pageName) + ' ---------'));
+                        formatMessage(res.messages, done);
+                    }
+                });
+            });
+        }
 
-			for (var x = 0; x < pages.length; x++) {
-				var childArgs = [
-					path.join(__dirname, 'phantomjs-script.js'),
-					pages[x]
-				];
+        function openPages(pages) {
 
-				openPage(childArgs, done, x);
-			}
-		};
+            for (var x = 0; x < pages.length; x++) {
+                var childArgs = [
+                    path.join(__dirname, 'phantomjs-script.js'),
+                    pages[x]
+                ];
 
-		var getAllPages = function(basePath, teams) {
+                openPage(childArgs, done, x);
+            }
+        }
 
-			if (global.argv.page !== undefined) {
-				openPages(pages.push(global.argv.page));
-			} else {
-				var counter = 0;
+        function getAllPages(basePath, teams) {
 
-				for (var team in teams) {
-					var dirPath = path.resolve(basePath + '../../../' + teams[team].views + '/default/pages'); //directory path
-					dir.readFiles(dirPath, {
-							match: /index.html/,
-							exclude: /^\./
-						}, function(err, content, next) {
-							if (err) throw err;
-							next();
-						},
-						function(err, files) {
-							if (err) throw err;
-							for (var i = 0; i < files.length; i++) {
-								pages.push(files[i].split('views/default/pages')[1].split('/index.html')[0])
-							}
-							counter++;
+            if (global.argv.page !== undefined) {
+                openPages(pages.push(global.argv.page));
+            } else {
+                var counter = 0;
 
-							if (counter === Object.keys(teams).length) {
-								openPages(pages);
-							}
-						});
-				}
-			}
-		};
+                for (var team in teams) {
+                    if (teams.hasOwnProperty(team)) {
+                        var dirPath = path.resolve(basePath + '../../../' + teams[team].views + '/default/pages'); //directory path
+                        dir.readFiles(dirPath, {
+                            match: /index.html/,
+                            exclude: /^\./
+                        }, function(err, content, next) {
+                            if (err) {
+                                throw err;
+                            }
+                            next();
+                        },
+                        function(err, files) {
+                            if (err) {
+                                throw err;
+                            }
+                            for (var i = 0; i < files.length; i++) {
+                                pages.push(files[i].split('views/default/pages')[1].split('/index.html')[0]);
+                            }
+                            counter++;
 
-		getAllPages(basePath, teams);
-	}
+                            if (counter === Object.keys(teams).length) {
+                                openPages(pages);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+
+        getAllPages(basePath, teams);
+    };
 };
-/*eslint-enable */
