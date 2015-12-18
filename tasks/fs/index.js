165b611a26cc3d5557988fb3d4d601904e49b5ce
alexbowen <alexbowen123@googlemail.com>
2015-12-18 10:37:44 +0000

    added jsDoc

--- tasks/fs/index.js
+++ tasks/fs/index.js
@@ -1,6 +1,19 @@
 'use strict';
 
+/**
+ * @module tasks/fs
+ */
 module.exports = {
+
+    /**
+     * delete
+     * @see module:tasks/fs/delete
+     */
     remove: require('./delete'),
+
+    /**
+     * copy
+     * @see module:tasks/fs/copy
+     */
     copy: require('./copy')
 };
